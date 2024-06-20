5060eddbd729639824c275af9717c7358fafbab8
EthanGarey <ethgarey@gmail.com>
2024-06-20 00:45:18 -0400

    Beta 0.0.1, Trying new things

---
+++ src/main/java/com/ethangarey/skybetter/SkyBetter.java
@@ -0,0 +1,10 @@
+package com.ethangarey.skybetter;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("skybetter")
+public class SkyBetter {
+    public SkyBetter() {
+        System.out.println("test");
+    }
+}
